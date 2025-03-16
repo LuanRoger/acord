ef6ae639900cd4a8506ab9d4d534b05408350161
ROG <luan.roger.2003@gmail.com>
2025-03-16 18:10:34 -0300

    feat: add CORS

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,13 @@
 import { serve } from "@hono/node-server";
 import { Hono } from "hono";
-import ActivityRoutes from "./routes/activity";
 import { logger } from "hono/logger";
+import { cors } from "hono/cors";
+import ActivityRoutes from "./routes/activity";
 
 const app = new Hono();
 
 app.use(logger());
+app.use(cors());
 
 app.route("/activity", ActivityRoutes);
 
